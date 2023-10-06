ce17af06ca9acd449bb070a239c912da863d6821
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2023-10-06 11:14:17 -0700

    Added a new method to calculate The Birthday Paradox using big numbers instead of recursion.  Also added a variety of debug outputs that are hidden by default to check the numbers at various points.

--- birthdayHap.py
+++ birthdayHap.py
@@ -1,26 +1,69 @@
+import math
 
 n = 365
 k = 2
 
-def getBirthdayOverlapProbability(numberOfPeople):
+# for debugging
+#totalNumerator = n
 
-	def multiplyBirthdayMultipliers(numberOfPeople):
+def getProbabilityOfOverlappingBirthdays_viaRecursion(numberOfPeople):
+
+	def getProbabilityOfAllDistinctBirthdays(numberOfPeople):
 		if numberOfPeople <= 1:
 			return 1
 		else:
 			numerator = n - numberOfPeople + 1
 			newMultiplier = numerator/n
+
+			# for debugging
 			#print(numerator)
-			return newMultiplier * multiplyBirthdayMultipliers(numberOfPeople-1)
+			#global totalNumerator
+			#totalNumerator *= numerator
+
+			return newMultiplier * getProbabilityOfAllDistinctBirthdays(numberOfPeople-1)
 	
-	return 1 - multiplyBirthdayMultipliers(numberOfPeople)
+	# for debugging - this method skips the calculation of n/n so starting the totalNumerator at n
+	#global totalNumerator	
+	#totalNumerator = n
+
+	return 1 - getProbabilityOfAllDistinctBirthdays(numberOfPeople) #, totalNumerator
+
+
+
+def getProbabilityOfOverlappingBirthdays_viaBigNumbers(numberOfPeople):
+	# n choose k
+	numberOfPossibleNonFavorableCollections = math.comb(n, numberOfPeople)
+	#print(f"# of non-favorable birthday collections: {numberOfPossibleNonFavorableCollections}")
+
+	# takes the number of collections and multiply it to yield the number of sequences
+	nonFavorableOutcomes = numberOfPossibleNonFavorableCollections * math.factorial(numberOfPeople)
+	#print(f"# of non-favorable birthday sequences:   {nonFavorableOutcomes}")
+	
+	# total possible sequences of birthdays (exponent)
+	totalPossibleSequencesOfBirthdays = n**numberOfPeople
+	#print(f"# of total possible birthday sequences:  {totalPossibleSequencesOfBirthdays}")
+	
+	probabilityOfAllDistinct = nonFavorableOutcomes/totalPossibleSequencesOfBirthdays
+	#print(f"Probability of all distinct birthdays:   {probabilityOfAllDistinct}")
+	
+	probabilityOfOverlap = 1 - probabilityOfAllDistinct
+	return probabilityOfOverlap
+
 
 
 if __name__ == '__main__':
-	while k < 24:
-		probability = getBirthdayOverlapProbability(k)
+	while k <= 23:
+		print(f"FOR {k} PEOPLE")
+		probability = getProbabilityOfOverlappingBirthdays_viaBigNumbers(k)
 		percentage = round(probability * 100, 2)
-		print(f"Put {k} people in a room and the odds that there will be a common birthday is {percentage}%.")
+		print(f"Chance of overlapping birthdays: {percentage}%. (via Big Number method)")
+		
+		probability = getProbabilityOfOverlappingBirthdays_viaRecursion(k)
+		percentage = round(probability * 100, 2)
+		#print(f"# of non-favorable birthday sequences:   {totalDistinctBirthdaySequences}")
+		print(f"Chance of overlapping birthdays: {percentage}%. (via Recursive method)")
+		print()
+
 		k+=1
 	input()
 
